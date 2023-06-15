b598b15de7d5459fce624fbe848bd086e7fdf774
ChefCarlos <carlind95@gmail.com>
2023-06-15 10:08:13 +0100

    ASG 3: Create GameliftAnywhere Testing Harness; Updated Sample Game to run SDK5; Deleted Local Testing Harness

--- Editor/GameLiftClientSettingsEditor.cs
+++ Editor/GameLiftClientSettingsEditor.cs
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-using AmazonGameLift.Runtime;
+using AmazonGameLiftPlugin.Core;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,33 +10,29 @@ namespace AmazonGameLift.Editor
     [CustomEditor(typeof(GameLiftClientSettings))]
     public sealed class GameLiftClientSettingsEditor : UnityEditor.Editor
     {
-        private SerializedProperty _isLocalTest;
-        private SerializedProperty _localPort;
+        
         private SerializedProperty _remoteUrl;
         private SerializedProperty _region;
         private SerializedProperty _poolClientId;
+        private SerializedProperty _isAnywhereTest;
+        private SerializedProperty _computeName;
+        private SerializedProperty _fleetID;
+        private SerializedProperty _fleetLocation;
+        private SerializedProperty _profileName;
 
-        private void OnEnable()
-        {
-            _isLocalTest = serializedObject.FindProperty(nameof(GameLiftClientSettings.IsLocalTest));
-            _localPort = serializedObject.FindProperty(nameof(GameLiftClientSettings.LocalPort));
-            _remoteUrl = serializedObject.FindProperty(nameof(GameLiftClientSettings.ApiGatewayUrl));
-            _region = serializedObject.FindProperty(nameof(GameLiftClientSettings.AwsRegion));
-            _poolClientId = serializedObject.FindProperty(nameof(GameLiftClientSettings.UserPoolClientId));
-        }
 
         public override void OnInspectorGUI()
         {
             serializedObject.Update();
             var targetSettings = (GameLiftClientSettings)target;
 
-            EditorGUILayout.PropertyField(_isLocalTest, new GUIContent("Local Testing Mode", "Enabling this to make sample game client connect to game server running on localhost"));
+            EditorGUILayout.PropertyField(_isAnywhereTest, new GUIContent("GameLift Anywhere", "Enabling this to make sample game client connect to game server running on an Anywhere fleet"));
 
             try
             {
-                if (targetSettings.IsLocalTest)
+                if (targetSettings.IsAnywhereTest)
                 {
-                    EditLocalMode(targetSettings);
+                    EditAnywhereMode(targetSettings);
                 }
                 else
                 {
@@ -49,14 +45,43 @@ namespace AmazonGameLift.Editor
             }
         }
 
-        private void EditLocalMode(GameLiftClientSettings targetSettings)
+        private void OnEnable()
         {
-            EditorGUILayout.LabelField("GameLift Local URL", targetSettings.LocalUrl);
-            EditorGUILayout.PropertyField(_localPort, new GUIContent("GameLift Local Port", "This port should match the port value defined in Local Testing"));
+            _remoteUrl = serializedObject.FindProperty(nameof(GameLiftClientSettings.ApiGatewayUrl));
+            _poolClientId = serializedObject.FindProperty(nameof(GameLiftClientSettings.UserPoolClientId));
+            _region = serializedObject.FindProperty(nameof(GameLiftClientSettings.AwsRegion));
+            _isAnywhereTest = serializedObject.FindProperty(nameof(GameLiftClientSettings.IsAnywhereTest));
+            _computeName = serializedObject.FindProperty(nameof(GameLiftClientSettings.computeName));
+            _fleetID = serializedObject.FindProperty(nameof(GameLiftClientSettings.fleetID));
+            _fleetLocation = serializedObject.FindProperty(nameof(GameLiftClientSettings.fleetLocation));
+            _profileName = serializedObject.FindProperty(nameof(GameLiftClientSettings.profileName));
+        }
 
-            if (targetSettings.LocalPort == 0)
+        private void EditAnywhereMode(GameLiftClientSettings targetSettings)
+        {
+            EditorGUILayout.PropertyField(_computeName, new GUIContent("Compute Name", "Fleet Compute Name"));
+            EditorGUILayout.PropertyField(_fleetID, new GUIContent("FleetID", "This Fleet Id should match the value generated in the AWS GameLift Console"));
+            EditorGUILayout.PropertyField(_fleetLocation, new GUIContent("Fleet Location", "This Location should match the value defined in the AWS GameLift Console"));
+            EditorGUILayout.PropertyField(_profileName, new GUIContent("Profile Name", "This Name should match the value defined on the users AWS Account"));
+
+            if (string.IsNullOrWhiteSpace(targetSettings.computeName))
             {
-                EditorGUILayout.HelpBox("Please set the GameLift Local port.", MessageType.Warning);
+                EditorGUILayout.HelpBox("Please set the Fleet Compute Name.", MessageType.Warning);
+            }
+
+            if (string.IsNullOrWhiteSpace(targetSettings.fleetID))
+            {
+                EditorGUILayout.HelpBox("Please set the FleetID.", MessageType.Warning);
+            }
+
+            if (string.IsNullOrWhiteSpace(targetSettings.fleetLocation))
+            {
+                EditorGUILayout.HelpBox("Please set the Fleet Location.", MessageType.Warning);
+            }
+            
+            if (string.IsNullOrWhiteSpace(targetSettings.profileName))
+            {
+                EditorGUILayout.HelpBox("Please set the AWS Profile Name.", MessageType.Warning);
             }
         }
 
@@ -65,6 +90,7 @@ namespace AmazonGameLift.Editor
             EditorGUILayout.PropertyField(_remoteUrl, new GUIContent("API Gateway Endpoint", "API Gateway URL"));
             EditorGUILayout.PropertyField(_region, new GUIContent("AWS Region", "AWS region used for communicating with Cognito and API Gateway"));
             EditorGUILayout.PropertyField(_poolClientId, new GUIContent("Cognito Client ID"));
+            EditorGUILayout.PropertyField(_region, new GUIContent("AWS Region"));
 
             if (string.IsNullOrWhiteSpace(targetSettings.ApiGatewayUrl))
             {
@@ -77,6 +103,11 @@ namespace AmazonGameLift.Editor
             }
 
             if (string.IsNullOrWhiteSpace(targetSettings.UserPoolClientId))
+            {
+                EditorGUILayout.HelpBox("Please set the User Pool Client ID.", MessageType.Warning);
+            }
+            
+            if (string.IsNullOrWhiteSpace(targetSettings.AwsRegion))
             {
                 EditorGUILayout.HelpBox("Please set the User Pool Client ID.", MessageType.Warning);
             }
